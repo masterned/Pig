83cae9511e722b249c74c920956ee9eaf4f8b992
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 01:13:12 -0400

    add toString to interface & reformat

--- src/edu/westga/cs6910/pig/model/Player.java
+++ src/edu/westga/cs6910/pig/model/Player.java
@@ -3,8 +3,10 @@ package edu.westga.cs6910.pig.model;
 /**
  * Player defines the interface for players in the game Pig.
  * 
- * @author CS6910
- * @version Summer 2021
+ * This class was started by CS6910 for Summer 2021
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-14
  */
 public interface Player {
 	/**
@@ -12,19 +14,20 @@ public interface Player {
 	 * 
 	 */
 	void takeTurn();
-	
+
 	/**
 	 * Resets the turn total to 0
 	 * 
 	 */
 	void resetTurnTotal();
-	
+
 	/**
 	 * Returns whether or not it is this player's turn
-	 * @return	True if and only if it is this player's turn
+	 * 
+	 * @return True if and only if it is this player's turn
 	 */
 	boolean getIsMyTurn();
-	
+
 	/**
 	 * Returns this Player's name.
 	 * 
@@ -34,20 +37,30 @@ public interface Player {
 
 	/**
 	 * Returns the total of all die rolls for this turn
-	 * @return	The total of all die rolls for this turn
+	 * 
+	 * @return The total of all die rolls for this turn
 	 */
 	int getTurnTotal();
-	
+
 	/**
 	 * Returns the total number of pips rolled by this player during the game
 	 * 
-	 * @return	The player's total score
+	 * @return The player's total score
 	 */
 	int getTotal();
-	
+
 	/**
 	 * Returns the number of pips showing on the die that were just rolled
-	 * @return	The number of pips showing on the die that were just rolled
+	 * 
+	 * @return The number of pips showing on the die that were just rolled
 	 */
 	String getDiceValues();
+
+	@Override
+	/**
+	 * Returns the String representation of the Player object
+	 * 
+	 * @return The String representation of the Player object
+	 */
+	String toString();
 }
