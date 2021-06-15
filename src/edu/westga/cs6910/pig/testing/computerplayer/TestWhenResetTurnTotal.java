85afd719757e99c8e10e26b2b64ceb3386347b84
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 17:48:13 -0400

    test ComputerPlayer's resetTurnTotal method

---
+++ src/edu/westga/cs6910/pig/testing/computerplayer/TestWhenResetTurnTotal.java
@@ -0,0 +1,39 @@
+package edu.westga.cs6910.pig.testing.computerplayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+
+/**
+ * Provides unit testing to verify the expected behavior of ComputerPlayer's
+ * resetTurnTotal method.
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-15
+ */
+public class TestWhenResetTurnTotal {
+
+	/**
+	 * When called on a new ComputerPlayer, the TurnTotal should stay 0.
+	 */
+	@Test
+	public void testWithNewComputerPlayerShouldSetTurnTotalTo0() {
+		ComputerPlayer testComputerPlayer = new ComputerPlayer();
+		testComputerPlayer.resetTurnTotal();
+		assertEquals(0, testComputerPlayer.getTurnTotal());
+	}
+
+	/**
+	 * When called on a ComputerPlayer with its turnTotal set, it should set its
+	 * turn total to 0.
+	 */
+	@Test
+	public void testWithPresetTurnTotalShouldSetTurnTotalTo0() {
+		ComputerPlayer testComputerPlayer = new ComputerPlayer();
+		testComputerPlayer.setTurnTotal(58);
+		testComputerPlayer.resetTurnTotal();
+		assertEquals(0, testComputerPlayer.getTurnTotal());
+	}
+}
