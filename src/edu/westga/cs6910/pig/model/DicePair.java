f9b0bdaf9f3ce153a9473a5ac2550b3d7837ad35
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 00:29:35 -0400

    create toString method & reformat file
    > the toString method is needed to test
    the constructor

--- src/edu/westga/cs6910/pig/model/DicePair.java
+++ src/edu/westga/cs6910/pig/model/DicePair.java
@@ -3,13 +3,15 @@ package edu.westga.cs6910.pig.model;
 /**
  * Represents a pair of dice
  * 
- * @author CS6910
- * @version Summer 2021
+ * This class was started by CS6910 for Summer 2021
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-15
  */
 public class DicePair {
 	private Die die1;
 	private Die die2;
-	
+
 	/**
 	 * Sets up dice pair to be ready for rolling
 	 */
@@ -17,7 +19,7 @@ public class DicePair {
 		this.die1 = new Die();
 		this.die2 = new Die();
 	}
-	
+
 	/**
 	 * Rolls each die
 	 */
@@ -25,20 +27,32 @@ public class DicePair {
 		this.die1.roll();
 		this.die2.roll();
 	}
-	
+
 	/**
 	 * Returns the number of pips showing on the first die
-	 * @return	The number of pips showing on the first die
+	 * 
+	 * @return The number of pips showing on the first die
 	 */
 	public int getDie1Value() {
 		return this.die1.getNumberOfPips();
 	}
-	
+
 	/**
 	 * Returns the number of pips showing on the second die
-	 * @return	The number of pips showing on the second die
+	 * 
+	 * @return The number of pips showing on the second die
 	 */
 	public int getDie2Value() {
 		return this.die2.getNumberOfPips();
+	}
+
+	@Override
+	/**
+	 * Returns the String representation of the DicePair
+	 * 
+	 * @return The String representation of the DicePair
+	 */
+	public String toString() {
+		return "die 1: " + this.die1.toString() + ", die 2: " + this.die2.toString();
 	}
 }
