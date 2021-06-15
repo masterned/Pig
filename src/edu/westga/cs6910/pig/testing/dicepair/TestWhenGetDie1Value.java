9457f76f8075c62169603cdcd2b46b39c8c696f4
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 00:47:53 -0400

    test DicePair's getDie1Value method

---
+++ src/edu/westga/cs6910/pig/testing/dicepair/TestWhenGetDie1Value.java
@@ -0,0 +1,27 @@
+package edu.westga.cs6910.pig.testing.dicepair;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.DicePair;
+
+/**
+ * Provides testing to verify the expected behavior of DicePair's getDie1Value
+ * method.
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-15
+ */
+public class TestWhenGetDie1Value {
+
+	/**
+	 * Method should return 1 when called on new DicePair.
+	 */
+	@Test
+	public void testWithNewDicePairShouldReturn1() {
+		DicePair testDicePair = new DicePair();
+		assertEquals(1, testDicePair.getDie1Value());
+	}
+
+}
