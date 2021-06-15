89a63cb66d8c2252ff28b3b9924e2db0d485a157
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 15:51:47 -0400

    fix ComputerPlayer's takeTurn scoring logic

--- src/edu/westga/cs6910/pig/model/ComputerPlayer.java
+++ src/edu/westga/cs6910/pig/model/ComputerPlayer.java
@@ -62,14 +62,14 @@ public class ComputerPlayer extends AbstractPlayer {
 			int die2Value = this.getThePair().getDie2Value();
 
 			if (die1Value == 1 || die2Value == 1) {
-				this.setTotal(this.getTotal() - this.getTurnTotal());
+				this.resetTurnTotal();
 				this.setIsMyTurn(false);
 				return;
 			} else {
 				this.setTurnTotal(this.getTurnTotal() + die1Value + die2Value);
-				this.setTotal(this.getTotal() + die1Value + die2Value);
 			}
 		}
+		this.setTotal(this.getTotal() + this.getTurnTotal());
 		this.setIsMyTurn(false);
 	}
 }
