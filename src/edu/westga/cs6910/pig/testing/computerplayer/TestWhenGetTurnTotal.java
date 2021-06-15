2c65f6c54f132742f12d026a0290d359fa307e9a
Spencer Dent <sd00070@my.westga.edu>
2021-06-15 17:32:51 -0400

    test ComputerPlayer's turnTotal accessor

---
+++ src/edu/westga/cs6910/pig/testing/computerplayer/TestWhenGetTurnTotal.java
@@ -0,0 +1,27 @@
+package edu.westga.cs6910.pig.testing.computerplayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+
+/**
+ * Provides unit testing to verify the expected behavior of ComputerPlayer's
+ * turnTotal accessor.
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-15
+ */
+public class TestWhenGetTurnTotal {
+
+	/**
+	 * When called on a new ComputerPlayer, the turnTotal should be 0.
+	 */
+	@Test
+	public void testWithNewComputerPlayerShouldHave0Points() {
+		ComputerPlayer testComputerPlayer = new ComputerPlayer();
+		assertEquals(0, testComputerPlayer.getTurnTotal());
+	}
+
+}
