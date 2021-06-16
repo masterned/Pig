54a1eb35c46224462a2a2b2c66539fd665892005
Spencer Dent <sd00070@my.westga.edu>
2021-06-16 00:58:52 -0400

    test Game's currentPlayer accessor
    > I really don't like the idea of testing
    against null, but I'm not sure there's any
    other way for me to test this method...

---
+++ src/edu/westga/cs6910/pig/testing/game/TestWhenGetCurrentPlayer.java
@@ -0,0 +1,28 @@
+package edu.westga.cs6910.pig.testing.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.ComputerPlayer;
+import edu.westga.cs6910.pig.model.Game;
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * Provides unit testing to verify the expected behavior of Game's currentPlayer
+ * accessor.
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-16
+ */
+public class TestWhenGetCurrentPlayer {
+
+	/**
+	 * When called on a new Game, should return null.
+	 */
+	@Test
+	public void testWithNewGameShouldReturnNull() {
+		Game testGame = new Game(new HumanPlayer("David Lightman"), new ComputerPlayer());
+		assertEquals(null, testGame.getCurrentPlayer());
+	}
+}
