6120792c274aa9b8f7710539c21ddc063fb28554
Spencer Dent <sd00070@my.westga.edu>
2021-06-16 03:08:52 -0400

    test HumanPlayer's addDiceToTurnTotal method

---
+++ src/edu/westga/cs6910/pig/testing/humanplayer/TestWhenAddDiceToTurnTotal.java
@@ -0,0 +1,28 @@
+package edu.westga.cs6910.pig.testing.humanplayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.HumanPlayer;
+
+/**
+ * Provides unit testing to verify the expected behavior of HumanPlayer's
+ * addDiceToTurnTotal method.
+ * 
+ * @author Spencer Dent
+ * @version 2021-06-16
+ */
+public class TestWhenAddDiceToTurnTotal {
+
+	/**
+	 * When called on a new HumanPlayer, turnTotal should be set to 2 (0 + 1 + 1).
+	 */
+	@Test
+	void testWithNewHumanPlayerShouldSetTurnTotalTo2() {
+		HumanPlayer testHumanPlayer = new HumanPlayer("David Lightman");
+		testHumanPlayer.addDiceToTurnTotal();
+		assertEquals(2, testHumanPlayer.getTurnTotal());
+	}
+
+}
